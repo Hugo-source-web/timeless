b45a3570660f9520a822a5d7cff1521fcdd9f59f
Hugo Rodriguez Garcia <hugaso3453@digitechfp.com>
2025-12-03 19:27:07 +0100

    Arreglo 6

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,6 @@ export default async function HomePage() {
       media: {
         where: {
           type: "IMAGE",
-          isHero: true,
         },
         orderBy: {
           sortOrder: "asc",
